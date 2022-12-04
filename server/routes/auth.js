1364d19728cdac29633cd5fe358942c55c7fc832
Ajay Pangare <ajaypangare8454@gmail.com>
2022-12-04 21:42:16 +0530

    till user api

---
+++ server/routes/auth.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const User = require("../models/user");
+
+const authRouter = express.Router();
+
+authRouter.post('/api/signup', async (req, res) => {
+   try {
+      const {name, email, profilePic} = req.body;
+
+      // email already exits ?
+      let user = await User.findOne({email: email});
+      if(!user){
+         user = new User({
+            email,
+            name,
+            profilePic,
+         });
+         user = await user.save();
+      }
+      res.json({user});
+      // store data
+
+
+   } catch (error) {
+      
+   }
+});
+
+module.exports = authRouter;
